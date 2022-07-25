9353332180f413e7d09a83714b11e2918abed99b
WaveOfCoding <waveformwstcrz@gmail.com>
2022-07-25 14:49:29 +0300

    feature: Create model

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import generics
+from api.models import Room
+from .serializers import RoomSerializer
 
-# Create your views here.
+
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+
+
+
